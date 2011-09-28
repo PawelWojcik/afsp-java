db20ae6e50fa8a822992b9235ee0d03d41337523
jinpu1220@gmail.com <jinpu1220@gmail.com@d6eb2e20-a827-ae36-e7d3-1fd274b23219>
2011-09-28 00:19:57 +0000

    新增部分FI函数及其依赖项。

---
+++ trunk/java/ca/mcgill/ece/libtsp/ST/ST.java
@@ -0,0 +1,15 @@
+package ca.mcgill.ece.libtsp.ST;
+
+public class ST {
+	public static int STKeyMatch(String str, String keyTable[]) {
+		int NOSTAR = 0;
+		int STAR = 1;
+		int NOMATCH	= -1;
+		
+		if(str == null) return NOMATCH;
+		for(String s : keyTable) {
+			
+		}
+		return NOMATCH;
+	}
+}
